b7e7fbcc232052515ac0d65aaa62827641fc9482
devshantanu10 <hereshantanu10@gmail.com>
2026-04-11 21:56:28 +0545

    Added files

--- main.py/form_app.py
+++ main.py/form_app.py
@@ -11,11 +11,22 @@ def submit():
     username = request.form.get("username")
     password = request.form.get("password")      # use name , define method , should match route 
 
-    if username == "shanta123" and password == "pass":
-          return render_template("form_welcome.html" , name = username )
-    else:
-         return "Invalid username or password"
+    # if username == "shanta123" and password == "pass":
+    #       return render_template("form_welcome.html" , name = username )
+    # else:
+    #      return "Invalid username or password"
+    valid_users = {
+        'admin': '123' , 
+        'shantanu' : 'pass' , 
+          'poudel' : "234"
+    }
     
+
+    if username in valid_users and password == valid_users[username]:
+        return render_template("form_welcome" , name = username)
+    else: 
+        return "invalid vcrendentials"
+
 
 if __name__ == '__main__':
     app.run(debug=True , port=5000) 
